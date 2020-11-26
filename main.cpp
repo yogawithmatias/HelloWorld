d100f5c1bf4ef3f7dddc18aa8438af18d3b70fef
Matias Medina <49899215+yogawithmatias@users.noreply.github.com>
2020-11-26 21:42:38 +0200

    be happy

--- main.cpp
+++ main.cpp
@@ -3,10 +3,16 @@
 using namespace std;
 
 
+void printHappy(){
+    cout << "Don't worry be happy!" << endl;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     cout << "hey hey" << endl;
     cout << "something";
     cout << "how are you?" << endl;
+    printHappy();
     return 0;
 }
+
